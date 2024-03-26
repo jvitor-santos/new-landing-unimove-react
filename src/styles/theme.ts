caec972896a4969fb7f72120775efd85cd491f2c
João Vitor <116177071+jvitor-santos@users.noreply.github.com>
2024-03-26 18:48:17 -0300

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,13 +3,15 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   styles: {
     global: {
+      *: {
+        scrollBehavior: 'smooth',
+      }
       html: {
         // fontSize: '62.5%',
       },
 
       body: {
         bg: '#EEEEEE',
-        scrollBehavior: 'smooth',
         fontFamily: 'Roboto, arial, sans-serif',
       },
     },
