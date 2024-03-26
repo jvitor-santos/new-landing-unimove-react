2e9d9b66af0369961fc4a215d21b3087cce554d1
João Vitor <116177071+jvitor-santos@users.noreply.github.com>
2024-03-26 18:56:03 -0300

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,7 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   styles: {
     global: {
-      *: {
+      '*': {
         scrollBehavior: 'smooth',
       },
       html: {
